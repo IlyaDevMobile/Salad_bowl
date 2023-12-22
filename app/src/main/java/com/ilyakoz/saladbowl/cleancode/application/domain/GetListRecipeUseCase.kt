def58d8c1441c702f3d52e7f98cde12a86079074
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-12-22 16:07:49 +0300

    check

--- app/src/main/java/com/ilyakoz/saladbowl/cleancode/application/domain/GetListRecipeUseCase.kt
+++ app/src/main/java/com/ilyakoz/saladbowl/cleancode/application/domain/GetListRecipeUseCase.kt
@@ -1,8 +1,6 @@
 package com.ilyakoz.saladbowl.cleancode.application.domain
 
-import androidx.lifecycle.LiveData
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
 
 class GetListRecipeUseCase(private val saladBowlRepository: SaladBowlRepository) {
 
